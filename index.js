b613eee861564a0cab6bea5e53039f8d84ce4d42
KevinAqila <farrelahmad.kevinaqila@gmail.com>
2025-07-17 21:04:18 +0700

    refactor

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/ideas", async(req, res) => {
+    try {
+        const queryParams = req.query;
+        const apiResponse = await axios.get("https://suitmedia-backend.suitdev.com/api/ideas", {
+            params: {
+                ...queryParams,
+                "append[]": "small_image",
+            },
+        });
+        res.json(apiResponse.data);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server proxy error" });
+    }
+});
+export default app;
